a5240396d2af098870074f6852a55629e542baa1
Gustavo Andrade <66392979+gustavoandradedeveloper@users.noreply.github.com>
2021-03-21 15:51:46 -0300

    Criação do Controlador Usuarios e atualização no nome da pasta do projeto
    Realizei a criação do controlador Usuarios e atualização no nome da pasta do projeto

---
+++ application/controllers/Usuarios.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit ('Ação não permitida ');
+
+class Usuarios extends CI_Controller{
+    
+    public function __construct() {
+        parent::__construct();
+        
+        //definir se há sessão
+    }
+    
+    //responsavel por chamar a view que vai apresentar no navegador os usuarios cadastrado -> vai mostrar em uma tabela
+    public function index(){
+        
+        //criando uma variavel do tipo array
+        //vai conter uma chaves chamada usuario que vai conter os usuarios cadastrado no banco de dados 
+        //para saber atraves do plugin como recurerar os dados do banco -> vai na documentação e procura 
+        
+        $data = array(
+            'usuarios' => $this->ion_auth->users()->result(), 
+        );//dentro do data já se tem informação para ser passada na view porem não ta sendo carregada a view para qual essas informações vai ser passsada 
+        
+        
+        //carrega a view header com o array data que contem todos os dados que vai ser mostrado na view
+        $this->load->view('layout/header',$data);
+        
+        //carrega a view usuarios
+        $this->load->view('usuarios/index');
+        
+        //carrega o footer do site
+        $this->load->view('layout/footer');
+    }
+    
+}
