e756da4391dedc2eb11928c7008d95cab7972596
Gustavo Andrade <66392979+gustavoandradedeveloper@users.noreply.github.com>
2021-03-21 08:58:02 -0300

    Trabalhando na parte ADM do sistema
    Realizei o fatiamento das parte do site ADM!

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,15 +1,18 @@
 <?php
+
 defined('BASEPATH') OR exit('Ação não permitida');
 
-class Home extends CI_Controller{
-    
+class Home extends CI_Controller {
+
     public function __construct() {
         parent::__construct();
     }
-    
-    public function index(){
-        
+
+    public function index() {
+
+        $this->load->view('layout/header');
         $this->load->view('home/index');
-        
+        $this->load->view('layout/footer');
     }
+
 }
