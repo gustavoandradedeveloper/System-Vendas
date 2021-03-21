8f6f7eeb72249a3e9712fc75e43e839b521da186
Gustavo Andrade <66392979+gustavoandradedeveloper@users.noreply.github.com>
2021-03-21 19:13:29 -0300

    Atualizando o Login do sistema
    Realizei atualizações na sessão de Login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,7 +5,31 @@ defined('BASEPATH') OR exit ('Ação não permitida');
 class Login extends CI_Controller{
 
     public function index(){
-        $this->load->view('layout/header');
-        $this->load->view('login/index');
+
+        /*
+             [email] => gugatavinho066@gmail.com
+            [password] => 123
+        */
+        
+        $identity = $this->security->xss_clean($this->input->post('email'));
+        $password = $this->security->xss_clean($this->input->post('password'));
+        $remember = False; // remember the user
+        if($this->ion_auth->login($identity, $password, $remember)){
+            redirect('home');
+        }else{
+            echo 'Erro de validação ';
+            $this->load->view('layout/header');
+            $this->load->view('login/index');
+        }
+
+        /*
+        echo'<pre>';
+        print_r($this->input->post());
+        exit();
+
+        printando na tela tudo que está sendo enviado pelo controlador expecificadamente para o metodo index
+        */
+
+        
     }
 }
