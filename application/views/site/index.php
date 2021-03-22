c90150084260804335f3cb6f48d12b93b6106277
Edder Luan <eluann100@gmail.com>
2021-03-21 21:57:36 -0300

    Atualização no index
    Atualização no arquivo Index.php

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -40,7 +40,7 @@
             <div class="container">
                 <div class="text-center">
                     <h2 class="section-heading text-uppercase">Portifólio</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h3 class="section-subheading text-muted">Acompanhe nossas criações.</h3>
                 </div>
                 <div class="row">
                     <div class="col-lg-4 col-sm-6 mb-4">
@@ -100,15 +100,15 @@
             <div class="container">
                 <div class="text-center">
                     <h2 class="section-heading text-uppercase">Sobre</h2>
-                    <h3 class="section-subheading text-muted">Um pouco mais sobre a nossa jornada.</h3>
+                    <h3 class="section-subheading text-muted">Um pouco mais sobre o nosso trabalho.</h3>
                 </div>
                 <ul class="timeline">
                     <li>
                         <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url('assets/assets/img/about/1.jpg" alt=""');?>" /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
-                                <h4>2009-2011</h4>
-                                <h4 class="subheading">Our Humble Beginnings</h4>
+                                <h4>2019</h4>
+                                <h4 class="subheading">Ingressamos no curso de Sistemas de Informação</h4>
                             </div>
                             <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
                         </div>
@@ -117,8 +117,8 @@
                         <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url('assets/assets/img/about/2.jpg" alt=""');?>" /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
-                                <h4>March 2011</h4>
-                                <h4 class="subheading">An Agency is Born</h4>
+                                <h4>Back-End</h4>
+                                <h4 class="subheading">Nossa forma de trabalho com linguágens e ferramentas Back-End</h4>
                             </div>
                             <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
                         </div>
@@ -127,8 +127,8 @@
                         <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url('assets/assets/img/about/3.jpg" alt=""');?>" /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
-                                <h4>December 2012</h4>
-                                <h4 class="subheading">Transition to Full Service</h4>
+                                <h4>Front-End</h4>
+                                <h4 class="subheading">Um pouco mais sobre nosso modelo de desenvolvimento Front-End e Design.</h4>
                             </div>
                             <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
                         </div>
@@ -137,8 +137,8 @@
                         <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url('assets/assets/img/about/4.jpg" alt=""');?>" /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
-                                <h4>July 2014</h4>
-                                <h4 class="subheading">Phase Two Expansion</h4>
+                                <h4>Dados e Segurança Digital</h4>
+                                <h4 class="subheading">Uma visão de como trabalhamos para otimizar a gestão de dados e como nos importamos com os dados dos nossos clientes.</h4>
                             </div>
                             <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
                         </div>
@@ -150,7 +150,7 @@
                                 <br />
                                 Da Nossa
                                 <br />
-                                história!
+                                História!
                             </h4>
                         </div>
                     </li>
