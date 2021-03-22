6640de8865b96cd846272749b2ec2457124842ee
AlefSouza10 <73562646+AlefSouza10@users.noreply.github.com>
2021-03-22 15:42:23 -0300

    Adequação da imagem
    Imagem alterada da sessão index (portfolio)

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -81,7 +81,7 @@
                                 <div class="portfolio-hover">
                                     <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                                 </div>
-                                <img class="img-fluid" src="<?php echo base_url('assets/assets/img/portfolio/03-thumbnail.jpg" alt="');?>" />
+                                <img class="img-fluid" src="<?php echo base_url('assets/assets/img/portfolio/03-Oficial.jpg" alt="');?>" />
                             </a>
                             <div class="portfolio-caption">
                                 <div class="portfolio-caption-heading">Sites e Blogs</div>
