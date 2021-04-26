6817c96c83406d807faec1f59262bff26c5cb26d
Gustavo Andrade <gustavoandradedeveloper@gmail.com>
2021-04-26 19:06:50 -0300

    Correção do Siderbar
    Realizei a correção do menu cadastro pois ele estava abrindo junto com o menu vendas!

--- application/views/layout/siderbar.php
+++ application/views/layout/siderbar.php
@@ -43,12 +43,12 @@
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-           aria-controls="collapseTwo">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseCadastro" aria-expanded="true"
+           aria-controls="collapseCadastro">
             <i class="fas fa-database"></i>
             <span>Cadastro</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div id="collapseCadastro" class="collapse" aria-labelledby="headingT" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">ESCOLHA UMA OPÇÃO:</h6>
                 <a class="collapse-item" href="#">Clientes</a>
