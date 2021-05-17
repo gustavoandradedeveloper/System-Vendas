8c6057d563ee452ff263bce2e701101b41618f3b
Gustavo Andrade <gustavoandradedeveloper@gmail.com>
2021-05-17 18:50:45 -0300

    Adicionando Comentários
    Realizei a adição de comentários na classe do sistema em controller

--- application/controllers/Sistema.php
+++ application/controllers/Sistema.php
@@ -7,7 +7,7 @@ class Sistema extends CI_Controller{
     public function __construct() {
         parent: : __construct();
 
-
+        //serve para fazer o redirecionamento da pagina AMD caso o usuario tente acessar a parte ADM sem está logado na plataforma
         if (!$this->ion_auth->logged_in())
         {
             $this->session->set_flashdata('info','Sua sessão expirou!');
