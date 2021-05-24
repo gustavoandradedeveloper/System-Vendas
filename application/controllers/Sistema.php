14cc48b727c7b6235cb47419fd7b301b0ff4bafd
Gustavo Andrade <gustavoandradedeveloper@gmail.com>
2021-05-24 19:37:04 -0300

    Efetuando a atualização de dados do Sistema
    Realizei a criação do array que vai capturar os dados vindo da view para salvar no banco de dados!

--- application/controllers/Sistema.php
+++ application/controllers/Sistema.php
@@ -20,37 +20,10 @@ class Sistema extends CI_Controller {
             'titulo' => 'Editar informações do sistema',
             'sistema' => $this->core_model->get_by_id('sistema', array('sistema_id' => 1)),
         );
-
-        /*
-          stdClass Object
-          (
-          [sistema_id] => 1
-          [sistema_razao_social] => Tech Manager
-          [sistema_nome_fantasia] => Tech Manager
-          [sistema_cnpj] => 17.184.837/0001-30
-          [sistema_ie] =>
-          [sistema_telefone_fixo] =>
-          [sistema_telefone_movel] =>
-          [sistema_email] => techmanager@contato.com.br
-          [sistema_site_url] => http://localhost/meus-projetos/tech-manager/site/
-          [sistema_cep] => 55730000
-          [sistema_endereco] => Derby
-          [sistema_numero] => 114
-          [sistema_cidade] => Vitoria de Santo Antão
-          [sistema_estado] => PE
-          [sistema_txt_ordem_servico] =>
-          [sistema_data_alteracao] => 2021-05-17 21:04:10
-          )
-
-          /*
-          echo'<pre>';
-          print_r($data['sistema']);
-          exit();
-         */
-
-        $this->form_validation->set_rules('sistema_razao_social', 'Razão social','required|min_length[10] |max_length[145]');
-        $this->form_validation->set_rules('sistema_nome_fantasia','Nome fantasia','required|min_length[10] |max_length[145]');
-        $this->form_validation->set_rules('sistema_cnpj', 'CNPJ', 'required|exact_length[18]');
+        $this->form_validation->set_rules('sistema_razao_social', 'Razão social', 'required|min_length[10] |max_length[145]');
+        $this->form_validation->set_rules('sistema_nome_fantasia', 'Nome fantasia', 'required|min_length[10] |max_length[145]');
+        $this->form_validation->set_rules('sistema_cnpj', 'CNPJ', 'required|max_length[25]');
+        $this->form_validation->set_rules('sistema_ie', '', 'required|exact_length[18]');
         $this->form_validation->set_rules('sistema_telefone_fixo', '', 'required|max_length[25]');
         $this->form_validation->set_rules('sistema_telefone_movel', '', 'required|max_length[25]');
         $this->form_validation->set_rules('sistema_email', '', 'required|valid_email|max_length[100]');
@@ -61,15 +34,64 @@ class Sistema extends CI_Controller {
         $this->form_validation->set_rules('sistema_cidade', 'Cidade', 'required|max_length[45]');
         $this->form_validation->set_rules('sistema_estado', 'UF', 'required|exact_length[2]');
         $this->form_validation->set_rules('sistema_txt_ordem_servico', 'Texto da ordem de serviços e vendas', 'max_length[500]');
-        
+
         // "exact_length" O campo {field} deve conter exatamente {param} caractere(s)
         //regras para validação -> vinda do arquivo form_validation_lang.php
-
-        
         if ($this->form_validation->run()) {
-            echo '<pre>';
-            print_r($this->input->post());
-            exit();
+            /*
+
+              echo '<pre>';
+              print_r($this->input->post());
+              exit();
+
+              stdClass Object
+              (
+              [sistema_id] => 1
+              [sistema_razao_social] => Tech Manager
+              [sistema_nome_fantasia] => Tech Manager
+              [sistema_cnpj] => 17.184.837/0001-30
+              [sistema_ie] =>
+              [sistema_telefone_fixo] =>
+              [sistema_telefone_movel] =>
+              [sistema_email] => techmanager@contato.com.br
+              [sistema_site_url] => http://localhost/meus-projetos/tech-manager/site/
+              [sistema_cep] => 55730000
+              [sistema_endereco] => Derby
+              [sistema_numero] => 114
+              [sistema_cidade] => Vitoria de Santo Antão
+              [sistema_estado] => PE
+              [sistema_txt_ordem_servico] =>
+              [sistema_data_alteracao] => 2021-05-17 21:04:10
+              )
+
+              /*
+              echo'<pre>';
+              print_r($data['sistema']);
+              exit();
+             */
+            //criando um array para pegar os dados vindo da view  e salvar no banco de dados
+            $data = elements(
+                    array(
+                'sistema_razao_social',
+                'sistema_nome_fantasia',
+                'sistema_cnpj',
+                'sistema_ie',
+                'sistema_telefone_fixo',
+                'sistema_telefone_movel',
+                'sistema_email',
+                'sistema_site_url',
+                'sistema_cep',
+                'sistema_endereco',
+                'sistema_numero',
+                'sistema_cidade',
+                'sistema_estado',
+                'sistema_txt_ordem_servico',
+                    ), $this->input->post()
+            );
+
+            $this->core_model->update('sistema', $data, array('sistema_id' => 1));
+            // vai ser igual '=> 1 ' porque só vai ter um registro desse nesse meu sistema e ele vai ser apenas atualizado
+            redirect('sistema');
         } else {
             $this->load->view('layout/header', $data);
             $this->load->view('sistema/index');
